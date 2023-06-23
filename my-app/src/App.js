f7d9e23c6aa8713c20d9ec5b64d83d1003652611
Jordan <wild_hunter0806@hotmail.com>
2023-06-23 12:46:25 +0800

    Create and import home.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header.js";
+import Home from "./Components/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <div className="App">
+        <Header />
+      </div>
+
+      <div className="home">
+        <Home />
+      </div>
+    </>
   );
 }
 
---
+++ my-app/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Menu, Transition } from "@headlessui/react";
+
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Home</h1>
+    </div>
+  );
+}
+
+export default Home;
