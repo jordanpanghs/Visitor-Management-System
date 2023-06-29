d8786e33cebef475a19d9653fc301ebf1e25056b
Jordan <wild_hunter0806@hotmail.com>
2023-06-29 12:20:30 +0800

    Implemented firebase

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxKvcfZcIQy42hjIwbo8jominxkAW6-J0",
+
+  authDomain: "visitor-management-syste-3f0f7.firebaseapp.com",
+
+  projectId: "visitor-management-syste-3f0f7",
+
+  storageBucket: "visitor-management-syste-3f0f7.appspot.com",
+
+  messagingSenderId: "780917214141",
+
+  appId: "1:780917214141:web:34bd993d8369b80a21e0d4",
+
+  measurementId: "G-N86RVED2HQ",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
