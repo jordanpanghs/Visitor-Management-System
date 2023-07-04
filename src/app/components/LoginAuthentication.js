a2d8e1f762431a4c699975dfdd22c4fa1a28adea
Jordan <wild_hunter0806@hotmail.com>
2023-07-05 00:22:27 +0800

    Improved login authentication routing

---
+++ src/app/components/LoginAuthentication.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../context/AuthContext";
+
+export default function LoginAuthentication() {
+  const router = useRouter();
+  const currentUser = useAuth();
+
+  useEffect(() => {
+    // if there is no authenticated user, redirect to login page
+    if (!currentUser) {
+      router.push("/login");
+    }
+  }, []);
+}
