a9d78169424b51dcc4150d6f3bdf702efe1a8a52
Jordan <wild_hunter0806@hotmail.com>
2023-07-09 14:09:04 +0800

    Installed react date time picker, written register special visitor page

--- src/app/components/RegisterSpecialVisitor.js
+++ src/app/components/RegisterSpecialVisitor.js
@@ -1,6 +1,22 @@
 "use client";
 
+import moment from "moment";
+import React from "react";
+
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+
 export default function RegisterSpecialVisitor() {
+  var yesterday = moment().subtract(1, "day");
+  function valid(current) {
+    return current.isAfter(yesterday);
+  }
+
+  let inputProps = {
+    className:
+      "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-32 lg:px-8">
@@ -17,51 +33,124 @@ export default function RegisterSpecialVisitor() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+            <div className="flex items-center justify-between space-x-10">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="text"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="icNumber"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Identity Card Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="icNumber"
+                    name="icNumber"
+                    type="text"
+                    autoComplete="text"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
             </div>
 
-            <div>
-              <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between space-x-10">
+              <div>
                 <label
-                  htmlFor="password"
+                  htmlFor="plateNumber"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Password
+                  Car Plate Number
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
+                <div className="mt-2">
+                  <input
+                    id="plateNumber"
+                    name="plateNumber"
+                    type="text"
+                    autoComplete="text"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
               </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+
+              <div>
+                <label
+                  htmlFor="telephoneNumber"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Telephone Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="telephoneNumber"
+                    name="telephoneNumber"
+                    type="text"
+                    autoComplete="tel"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between space-x-10">
+              <div>
+                <label
+                  htmlFor="visitDateTime"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Visiting Date & Time
+                </label>
+                <div className="mt-2">
+                  <Datetime
+                    inputProps={inputProps}
+                    isValidDate={valid}
+                    id="visitDateTime"
+                    name="visitDateTime"
+                    type="time"
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="visitPurpose"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Visiting Purpose
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="visitPurpose"
+                    name="visitPurpose"
+                    type="text"
+                    autoComplete="off"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
             </div>
 
