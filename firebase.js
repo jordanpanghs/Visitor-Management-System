ccff557be7671e6fef32c1d860c50a25ff3ee5a2
Jordan <wild_hunter0806@hotmail.com>
2023-08-15 12:30:33 +0800

    Added firebase cloud storage to config

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -20,6 +21,6 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-
+export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
