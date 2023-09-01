db38fe9c39e6263ed6a30022212b85b52b8077f0
Jordan <wild_hunter0806@hotmail.com>
2023-09-01 22:41:46 +0800

    Removed metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,11 +4,6 @@ import "./globals.css";
 import LoginAuthentication from "./components/LoginAuthentication";
 import { AuthProvider } from "./context/AuthContext";
 
-export const metadata = {
-  title: "Visitor Management System",
-  description: "By Jordan Pang",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full bg-white">
